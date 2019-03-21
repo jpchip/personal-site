26a4294c19e5106503189a714b9f6919d8a7806d
Jared Chapiewsky <chapiewsky@earthlinginteractive.com>
2019-03-21 10:33:34 -0500

    include bootstrap js as script tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-import 'bootstrap/dist/js/bootstrap.js';
 
 import Header from "./header"
 import '../styles/styles.scss';
+import Helmet from 'react-helmet'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -19,6 +19,10 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+          <Helmet>
+			  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" />
+			  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous" />
+          </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div>
           <main className="container mb-5 mt-3">{children}</main>
