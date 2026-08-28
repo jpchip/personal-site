269e6af662d4a86ace2e9d1d5f70146038f256de
Jared Chapiewsky <jpchapiewsky@gmail.com>
2026-08-27 22:35:29 -0500

    clean up visual issues

--- src/components/social.js
+++ src/components/social.js
@@ -18,21 +18,20 @@ const Social = () => {
   `)
 
   return (
-    <ul className="social-icons-list list-group list-group-horizontal list-group-flush">
+    <div className="social-icons-list list-group list-group-horizontal list-group-flush">
       {data.allSocialJson.edges.map(social => (
-        <li key={social.node.id}>
-          <a
-            href={social.node.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            title={social.node.title}
-            className="list-group-item bg-transparent list-group-item-action text-center flex-fill"
-          >
-            <i className={social.node.icon} />
-          </a>
-        </li>
+        <a
+          key={social.node.id}
+          href={social.node.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          title={social.node.title}
+          className="list-group-item bg-transparent list-group-item-action text-center flex-fill"
+        >
+          <i className={social.node.icon} />
+        </a>
       ))}
-    </ul>
+    </div>
   )
 }
 
